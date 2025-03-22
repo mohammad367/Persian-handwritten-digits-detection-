048b45a53f0a9f8e06269d6ed1026b5a4d6eab38
mohammad nasri <mohammad@LAPTOP-IFBQTM34.>
2025-03-22 12:44:44 +0330

    simply main works

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import cv2
 import numpy as np
 import io
 
-from image_processing import detect_and_label_digits
-from services.image_service_knn import process_image as knn_image_processor 
+# from image_processing import detect_and_label_digits
+# from services.image_service_knn import process_image as knn_image_processor 
 
 app = FastAPI()
 
 
 @app.post("/upload/")
 async def upload_image(file: UploadFile = File(...)):
-    print('/n/nhi/n/n')
-    # labeled_image= await knn_image_processor(file)
-    
-    # _, encoded_image = cv2.imencode(".jpg", labeled_image)
-    # return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
+    contents = await file.read()
+    image = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.IMREAD_COLOR)
+
+    if image is None:
+        raise HTTPException(status_code=400, detail="Invalid image file")
+
+    labeled_image = image# detect_and_label_digits(image)
+
+    _, encoded_image = cv2.imencode(".png", labeled_image)
+    return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
 
 if __name__ == "__main__":
     import uvicorn
